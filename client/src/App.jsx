3e5bb90e594d2e5e624fd0650788d27d84c9a659
Sushant Nain <sushantnain0123@gmail.com>
2025-06-01 09:44:55 +0530

    Implemented user management features; Forgot and Reset Password pages with UI and logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import ForgotPassword from './pages/ForgotPassword'
-import OTP from './pages/OTP'
-import ResetPassword from './pages/ResetPassword'
-import {ToastContainer} from 'react-toastify'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import OTP from "./pages/OTP";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./store/slices/authSlice";
+import { fetchAllUsers } from "./store/slices/userSlice";
 
+const App = () => {
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+    // // dispatch(fetchAllBooks());
+    // if (isAuthenticated && user?.role === "User") {
+    //   dispatch(fetchUserBorrowedBooks());
+    // }
+    if (isAuthenticated && user?.role === "Admin") {
+      dispatch(fetchAllUsers());
+      // dispatch(fetchAllBorrowedBooks());
+    }
+  }, [isAuthenticated]);
 
-function App() {
-  return <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/password/forgot" element={<ForgotPassword />} />
-      <Route path="/otp-verification/:email" element={<OTP />} />
-      <Route path="/password/reset/:token" element={<ResetPassword />} />
-    </Routes>
-    <ToastContainer theme='dark'/>
-  </Router>
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/otp-verification/:email" element={<OTP />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+      </Routes>
+      <ToastContainer theme="dark" />
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import logo_p_nn from "../assets/logo_purple_noName.png";
+import logo_White_n from "../assets/Logo_white_withName.png";
+import { useDispatch, useSelector } from "react-redux";
+import { forgotPassword, resetAuthSlice } from "../store/slices/authSlice";
+import { toast } from "react-toastify";
+import { Link, Navigate } from "react-router-dom";
 
-function ForgotPassword() {
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error, message, user, isAuthenticated } = useSelector(
+    (state) => state.auth
+  );
+
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    dispatch(forgotPassword(email));
+  };
+
+  useEffect(() => {
+    if (message) {
+      toast.success(message);
+      dispatch(resetAuthSlice());
+    }
+    if (error) {
+      toast.error(error);
+      dispatch(resetAuthSlice());
+    }
+  }, [dispatch, isAuthenticated, error, loading]);
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <>
+      <div className="flex flex-col justify-center md:flex-row h-screen">
+        {/* LEFT SECTION */}
+        <div className="hidden w-full md:w-1/2 bg-[#511d81] text-white md:flex flex-col items-center justify-center p-8 rounded-tr-[80px] rounded-br-[80px]">
+          <div className="text-center h-[450px]">
+            <div className="flex h-40 justify-center mb-12">
+              <img
+                src={logo_White_n}
+                alt="logo"
+                className="mb-12 h-44 w-auto"
+              />
+            </div>
+            <h3 className="text-gray-100 mb-12 max-w-[320px] mx-auto text-3xl font-medium leading-10">
+              "Your premier digital library for borrowing and reading books"
+            </h3>
+          </div>
+        </div>
+        {/* RIGHT SECTION */}
+        <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-8 relative">
+          <Link
+            to={"/login"}
+            className="border-2 border-black bg-white rounded-3xl font-bold w-44 py-2 px-4 fixed top-10 -left-28 hover:bg-[#9f65d5] hover:text-white transition duration-300 text-end"
+          >
+            Back
+          </Link>
+          <div className="w-full max-w-sm">
+            <div className="flex justify-center mb-12">
+              <div className="rounded-full flex items-center justify-center">
+                <img src={logo_p_nn} alt="logo" className="h-24 w-auto" />
+              </div>
+            </div>
+            <h1 className="text-4xl font-medium text-center mb-5 overflow-hidden">
+              Forgot Password
+            </h1>
+            <p className="text-gray-800 text-center mb-12">
+              Please enter your email
+            </p>
+            <form onSubmit={handleForgotPassword}>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  className="w-full px-4 py-3 border border-black rounded-md focus:outline-none"
+                />
+              </div>
+              <button
+                type="submit"
+                className="border-2 mt-5 border-[#511d81] w-full font-semibold bg-[#511d81] text-white py-2 rounded-lg hover:bg-[#8a4ec2] transition"
+                disabled={loading ? true : false}
+              >
+                RESET PASSWORD
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
